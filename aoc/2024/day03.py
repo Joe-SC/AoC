35a58e9d18d83fd2171653e56e483fa3827e6856
Joseph Sy Chu <Joe.SyChu@gmail.com>
2024-12-03 21:08:14 +0000

    day 3

--- aoc/2024/day03.py
+++ aoc/2024/day03.py
@@ -1,20 +1,46 @@
 """https://adventofcode.com/2024/day/3"""
 
-import os
+from aoc_utils import fetch_input_data
+import logging
+logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
+import re
 
-with open(os.path.join(os.path.dirname(__file__), "inputs/day03_input.txt")) as f:
-    actual_input = f.read()
+logger = logging.getLogger(__name__)
 
+actual_input = fetch_input_data(2024, 3)
+sample_input_1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+sample_input_2="xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
-sample_input = """xxx"""
+def solve_part_1(inputs: str) -> int:
+    pattern = r'mul\((\d{1,3}),(\d{1,3})\)'    
+    total = 0
+    for m in re.finditer(pattern, inputs):
+        total += int(m.group(1)) * int(m.group(2))
+    return total
 
+def solve_part_2(inputs: str) -> int:
+    pattern = r'(?:mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\))'
+    total = 0
+    toggle = 1
+    for m in re.finditer(pattern, inputs):
+        if m.group(0).startswith('don'):
+            logger.debug(f"m.group(0): {m.group(0)}")
+            toggle = 0
+            logger.debug(f"toggle: {toggle}")
+        elif m.group(0).startswith('do'):
+            logger.debug(f"m.group(0): {m.group(0)}")
+            toggle = 1
+            logger.debug(f"toggle: {toggle}")
+        elif m.group(0).startswith('mul'):
+            logger.debug(f"m.group(0): {m.group(0)}")
+            total += toggle * int(m.group(1)) * int(m.group(2))
+    return total
 
-def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
-
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}\n")
-
-
-solve(sample_input)
-# solve(actual_input)
+if __name__ == "__main__":
+    # Part 1
+    assert solve_part_1(sample_input_1) == 161  
+    print("Part 1:", solve_part_1(actual_input))
+    # Part 2
+    assert solve_part_2(sample_input_2) == 48
+    print("Part 2:", solve_part_2(actual_input))
